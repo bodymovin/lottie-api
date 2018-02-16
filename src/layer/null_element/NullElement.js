d3d0da3201d323dd5c6cd5f7b63e79042b3e5a8e
hernan <hernantorrisi@gmail.com>
2018-02-15 20:51:54 -0500

    null element

--- src/layer/null_element/NullElement.js
+++ src/layer/null_element/NullElement.js
@@ -1,11 +1,23 @@
 var LayerBase = require('../LayerBase');
 
-function NullElement() {
+function NullElement(element) {
+
+	var instance = {};
+
+	var state = {
+		element: element,
+		properties: _buildPropertyMap()
+	}
+
+	function _buildPropertyMap() {
+		return [
+		]
+	}
 
 	var methods = {
 	}
 
-	return Object.assign({}, LayerBase(element), methods);
+	return Object.assign(instance, LayerBase(state), methods);
 }
 
 module.exports = NullElement;
