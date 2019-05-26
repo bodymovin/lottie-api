793fcfc4d8f2da1b868977c1a2c36eaf696e28c0
airnan <hernantorrisi@gmail.com>
2019-05-26 09:36:13 -0400

    fixes #12 Image layer instantiation fix

--- src/layer/image/ImageElement.js
+++ src/layer/image/ImageElement.js
@@ -2,10 +2,21 @@ var LayerBase = require('../LayerBase');
 
 function Image(element) {
 
+	var state = {
+		element: element,
+		parent: parent,
+		properties: _buildPropertyMap()
+	}
+
+	function _buildPropertyMap() {
+		return [
+		]
+	}
+	
 	var methods = {
 	}
 
-	return Object.assign({}, LayerBase(element), methods);
+	return Object.assign({}, LayerBase(state), methods);
 }
 
 module.exports = Image;
